2fdaea514fd205ec72e5400f5d7c2f332febfd4d
Blix <sblixenkrone@me.com>
2019-02-20 16:10:20 +0100

    IO Mem ready pdf working

--- server/storage.go
+++ server/storage.go
@@ -1,14 +1,12 @@
 package server
 
 import (
+	"bytes"
 	"fmt"
-	"net/http"
 	"os"
 
-	"github.com/aws/aws-sdk-go/aws/credentials"
-
 	"github.com/aws/aws-sdk-go/aws"
-
+	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
@@ -19,7 +17,7 @@ const (
 )
 
 // NewUpload -
-func NewUpload(fileDir string) error {
+func NewUpload(file []byte, dateStamp string) error {
 	s, err := session.NewSession(&aws.Config{
 		Region:      aws.String(s3Region),
 		Credentials: credentials.NewStaticCredentials(os.Getenv("AWS_ACCESS_KEY_ID"), os.Getenv("AWS_SECRET_ACCESS_KEY"), ""),
@@ -28,29 +26,34 @@ func NewUpload(fileDir string) error {
 		return err
 	}
 	sess := session.Must(s, err)
-	if err := uploader(sess, fileDir); err != nil {
+	if err := uploader(sess, file, dateStamp); err != nil {
 		return err
 	}
 	return nil
 }
 
 // Uploader S3 uploader
-func uploader(s *session.Session, fileDir string) error {
+func uploader(s *session.Session, file []byte, dateStamp string) error {
 	uploader := s3manager.NewUploader(s)
-	file, err := os.Open(fileDir)
-	if err != nil {
-		return fmt.Errorf("Failed to open file: %q, %v", fileDir, err)
-	}
-	defer file.Close()
+	fileName := dateStamp[:7] + ".pdf"
+	// file, err := os.Open(fileDir)
+	// if err != nil {
+	// 	return fmt.Errorf("Failed to open file: %q, %v", fileDir, err)
+	// }
+	// defer file.Close()
 
-	stats, _ := file.Stat()
-	size := stats.Size()
-	buffer := make([]byte, size)
+	// if err != nil {
+	// 	return fmt.Errorf("Failed to open file: %q, %v", fileDir, err)
+	// }
+	// stats, _ := file.Stat()
+	// size := stats.Size()
+	// buffer := make([]byte, size)
+
 	result, err := uploader.Upload(&s3manager.UploadInput{
-		Body:                 file,
-		Bucket:               aws.String(s3Bucket),
-		Key:                  aws.String(fileDir),
-		ContentType:          aws.String(http.DetectContentType(buffer)),
+		Body:   bytes.NewBuffer(file),
+		Bucket: aws.String(s3Bucket),
+		Key:    aws.String(string(fileName)),
+		// ContentType:          aws.String(http.DetectContentType(buffer)),
 		ServerSideEncryption: aws.String("AES256"),
 	})
 	if err != nil {
